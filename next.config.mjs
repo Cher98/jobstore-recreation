d89e57e7542857250be216a8ab09e063096b5849
cherilynfaye08 <cherilynfaye08@gmail.com>
2025-08-06 16:14:48 +0800

    chore: added build script for github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/jobstore-recreation",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
