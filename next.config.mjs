dac0ccb1cdfc7879cc063489e9676efa86594fbe
cherilynfaye08 <cherilynfaye08@gmail.com>
2025-08-06 16:22:26 +0800

    chore: fix deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   basePath: "/jobstore-recreation",
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
